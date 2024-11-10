94acbaa50392c30eb3b7c6d257bd4dc055e555e0
Michał Kaczmarek <252940@edu.p.lodz.pl>
2024-11-11 00:56:48 +0100

    Correlation and linear regression added

--- ZAD 1/main.py
+++ ZAD 1/main.py
@@ -1,8 +1,10 @@
 from get_data import get_data
 from tables import tables
 from hist_box import hist_box
+from scatter_plots import scatter_plots
 
 data = get_data("./data/data1.csv")
 
 tables(data)
 hist_box(data)
+scatter_plots(data)
---
+++ ZAD 1/scatter_plots.py
@@ -0,0 +1,41 @@
+from tables import mean
+from hist_box import get_column
+from tables import mean
+import math
+
+def correlation(x, y, x_mean, y_mean):
+    numerator = 0
+    denominator_x = 0
+    denominator_y = 0
+
+    for i in range(len(x)):
+        x_part = x[i] - x_mean
+        y_part = y[i] - y_mean
+        numerator += x_part * y_part
+        denominator_x += pow(x_part, 2)
+        denominator_y += pow(y_part, 2)
+
+    return round(numerator / math.sqrt(denominator_x * denominator_y), 2)
+
+def linear_regression(x, y, x_mean, y_mean):
+    numerator = 0
+    denominator = 0
+
+    for i in range(len(x)):
+        x_part = x[i] - x_mean
+        y_part = y[i] - y_mean
+        numerator += x_part * y_part
+        denominator += pow(x_part, 2)
+    
+    b = numerator / denominator
+    a = y_mean - b * x_mean
+
+    return a, b
+
+def pairs(data):
+    print(correlation(get_column(data, 0), get_column(data, 1), mean(data, 0), mean(data, 1)))
+    linear_regression(get_column(data, 0), get_column(data, 1), mean(data, 0), mean(data, 1))
+
+def scatter_plots(data):
+    pairs(data)
+    return True
