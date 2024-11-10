64c8e71747c01424c943e26c3e5a3561fb8c4355
Michał Kaczmarek <252940@edu.p.lodz.pl>
2024-11-11 00:12:10 +0100

    Box plots added

--- ZAD 1/hist_box.py
+++ ZAD 1/hist_box.py
@@ -9,6 +9,15 @@ def get_column(data, col_index):
 
     return column
 
+def get_column_for_species(data, col_index, species_id):
+    column = []
+
+    for i in range(len(data)):
+        if data[i][-1] == species_id:
+            column.append(data[i][col_index])
+
+    return column
+
 def get_bins(start, end):
     result = []
 
@@ -26,12 +35,11 @@ def get_bins(start, end):
 
     return result
 
-def hist(data, title, description):
-    col_index = description["index"]
+def hist(title, description):
     bins_list = description["bins"]
     xlabel = description["xlabel"]
 
-    dataset = get_column(data, col_index)
+    dataset = description["data"]
 
     plt.hist(dataset, bins=bins_list, edgecolor='black')
 
@@ -44,30 +52,70 @@ def hist(data, title, description):
 def hist_all(data):
     characteristics = {
         'Długość działki kielicha': {
-            "index": 0,
+            "data": get_column(data, 0),
             "xlabel": "Długość (cm)",
             "bins": get_bins(find_min(data, 0), find_max(data, 0))
         },
         'Szerokość działki kielicha': {
-            "index": 1,
+            "data": get_column(data, 1),
             "xlabel": "Szerokość (cm)",
             "bins": get_bins(find_min(data, 1), find_max(data, 1))
         },
         'Długość płatka': {
-            "index": 2,
+            "data": get_column(data, 2),
             "xlabel": "Długość (cm)",
             "bins": get_bins(find_min(data, 2), find_max(data, 2))
         },
         'Szerokość płatka': {
-            "index": 3,
+            "data": get_column(data, 3),
             "xlabel": "Szerokość (cm)",
             "bins": get_bins(find_min(data, 3), find_max(data, 3))
         }
     }
 
     for title, description in characteristics.items():
-        hist(data, title, description)
+        hist(title, description)
 
+def box(title, description):
+    dataset = [item for item in description.values()]
+
+    plt.boxplot(dataset)
+
+    plt.xticks([i + 1 for i in range(len(dataset))], list(description.keys()))
+
+    plt.title(title)
+    plt.xlabel('Gatunek')
+    plt.ylabel("Długość (cm)" if title[0] == "D" else "Szerokość (cm)")
+
+    plt.show()
+
+def box_all(data):
+    characteristics = {
+        'Długość działki kielicha': {
+            "setosa": get_column_for_species(data, 0, 0),
+            "versicolor": get_column_for_species(data, 0, 1),
+            "virginica": get_column_for_species(data, 0, 2),
+        },
+        'Szerokość działki kielicha': {
+            "setosa": get_column_for_species(data, 1, 0),
+            "versicolor": get_column_for_species(data, 1, 1),
+            "virginica": get_column_for_species(data, 1, 2),
+        },
+        'Długość płatka': {
+            "setosa": get_column_for_species(data, 2, 0),
+            "versicolor": get_column_for_species(data, 2, 1),
+            "virginica": get_column_for_species(data, 2, 2),
+        },
+        'Szerokość płatka': {
+            "setosa": get_column_for_species(data, 3, 0),
+            "versicolor": get_column_for_species(data, 3, 1),
+            "virginica": get_column_for_species(data, 3, 2),
+        },
+    }
+
+    for title, description in characteristics.items():
+        box(title, description)
 
 def hist_box(data):
     hist_all(data)
+    box_all(data)
