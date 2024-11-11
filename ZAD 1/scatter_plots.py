68876ea23702f76a39e1fdcc87d42828438b90b4
Michał Kaczmarek <252940@edu.p.lodz.pl>
2024-11-11 01:39:37 +0100

    Scatter plots

--- ZAD 1/scatter_plots.py
+++ ZAD 1/scatter_plots.py
@@ -1,7 +1,13 @@
+import matplotlib.pyplot as plt
 from tables import mean
 from hist_box import get_column
 from tables import mean
 import math
+
+def characteristics(column_index):
+    data = ["Długość działki kielicha", "Szerokość działki kielicha", "Długość płatka", "Szerokość płatka"]
+
+    return data[column_index]
 
 def correlation(x, y, x_mean, y_mean):
     numerator = 0
@@ -32,10 +38,43 @@ def linear_regression(x, y, x_mean, y_mean):
 
     return a, b
 
-def pairs(data):
-    print(correlation(get_column(data, 0), get_column(data, 1), mean(data, 0), mean(data, 1)))
-    linear_regression(get_column(data, 0), get_column(data, 1), mean(data, 0), mean(data, 1))
+def plot(x, y, x_mean, y_mean, x_label, y_label):
+    a, b = linear_regression(x, y, x_mean, y_mean)
+
+    plt.scatter(x, y, color='blue')
+
+    y_line = [b * xi + a for xi in x]
+    plt.plot(x, y_line, color='red',)
+
+    plt.xlabel(x_label + " (cm)")
+    plt.ylabel(y_label + " (cm)")
+
+    r = round(correlation(x, y, x_mean, y_mean), 2)
+    plt.title(f'r = {r}; y = {round(b, 1)}x + {round(a, 1)}')
+
+    plt.show()
+
+def all_plots(data):
+    pairs = []
+
+    for i in range(len(data[0]) - 1):
+        for j in range(i + 1, len(data[0]) - 1):
+            pairs.append([i, j])
+
+    for pair in pairs:
+        x, y = pair
+
+        x_column = get_column(data, x)
+        y_column = get_column(data, y)
+        x_mean = mean(data, x)
+        y_mean = mean(data, y)
+        x_label = characteristics(x)
+        y_label = characteristics(y)
+
+        plot(x_column, y_column, x_mean, y_mean, x_label, y_label)
+        
 
 def scatter_plots(data):
-    pairs(data)
+    all_plots(data)
+
     return True
